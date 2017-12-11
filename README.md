677bf6a6aeebb8cbad93766c1bfacc6c83419c25
Joseph Semlak <semlak@gmail.com>
2017-12-10 21:16:04 -0600

    basically working, attachPoints need restructuring

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # week-4-game
+
+### Overview
+
+This is a small webapp/game I programmed as a homework assignment for a program I was completing. 
+
+The game is written as a simple web-application where the majority of functionality is in JavaScript, with jQuery used to handle the page maniuplation. The program also uses some stying with [Bootstrap 4](https://getbootstrap.com/) and [Material Design for Bootstrap](https://mdbootstrap.com/components/cards/).
+
+As far as the game goes, it is a really basic Role Playing Game (RPG). Although the characters are taken from the Star Wars Universe, the functionality of the game is not otherwise based on that story.
+
+A playable version of the game has been deployed via GitHub Pages, at [https://semlak.github.io/week-4-game/](https://semlak.github.io/week-4-game/)
+
+#### Basic flow of game play: 
+1. When the game starts, the player choses a character to play as. They will play as that character for the rest of the game
+	* If the player choses to reset the game, they can chose a different character to play as.
+2. From that point, the goal of the game is to defeat all other characters, one at a time.
+	* The player selects one of the remaining characters to attack.
+	* The player then attacks this character until either one of them is defeated.
+	* If the player's character is defeated, the game is over.
+	* If the opponent character is defeated, the player must continue until all remaining characters are defeated.
+
+#### How a battle between two characters works:
+* While attacking, the player's character's attack points inflicted on the opponent increase with each single attack. 
+* Meanwhile, the opponent character's attack power remains constant. 
+* Each character inflicts their attack power on the other character, which results in an associated reduction in that character's health points.
+
+#### Other notes:
+* It is not necessarily possible to defeat all characters in any given order.
+* It should be possible to win with any of the characters, but that the order to defeat the other characters may need to be chosen wisely.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,30 +26,34 @@ function deepCLoneOfArrayOfOjects(arrayOfOjbects) {
 const GameData = {
 	characters: [{
 			name: "Luke",
+			_id: "luke",
 			description: "Area moister farmer",
 			initialHP: 100,
-			intialAP: 8,
+			initialAP: 14,
 			counterAP: 8,
 			image: "./assets/images/luke1.jpg"
 		}, {
 			name: "Jabba the Hutt",
+			_id: "jabba",
 			description: "A slimy piece of worm-ridden filth",
 			initialHP: 120,
-			intialAP: 8,
+			initialAP: 8,
 			counterAP: 12,
 			image: "./assets/images/jabba.jpg"
 		}, {
 			name: "Greedo",
+			_id: "greedo",
 			description: "Local bounty hunter",
 			initialHP: 150,
-			intialAP: 8,
+			initialAP: 8,
 			counterAP: 20,
 			image: "./assets/images/greedo.png"
 		}, {
 			name: "Han",
+			_id: "han",
 			description: "He ALWAYS shoots first!",
 			initialHP: 180,
-			intialAP: 8,
+			initialAP: 8,
 			counterAP: 25,
 			image: "./assets/images/han.jpg"
 		}
@@ -59,7 +63,7 @@ const GameData = {
 
 
 var Character = class {
-	// constructor(name, intialHP, intialAP, counterAP, description) {
+	// constructor(name, initialHP, initialAP, counterAP, description) {
 	// 	this.name = name;
 	// 	this.healthPoints = initialHP;
 	// 	this.initialAP = initialAP;
@@ -76,14 +80,16 @@ var Character = class {
 		this.counterAP = charData.counterAP;
 		this.description = charData.description;
 		this.image = charData.image;
+		this._id = charData._id;
 	}
 
 	isDefeated() {
-		return (this.healthPoints > 0);
+		return (!(this.healthPoints > 0));
 	}
 
 	attackWithCharacter(opponent) {
-		opponent.receiveAttackFromPlayer(this);
+		// opponent.receiveAttackFromPlayer(this);
+		opponent.healthPoints = opponent.healthPoints - this.currentAttackPower; 
 		//opponent healthPoints will be updated accordingly. If opponent is not defeated, then their counterAttack will affect our character
 
 		if (!(opponent.isDefeated())) {
@@ -97,8 +103,11 @@ var Character = class {
 
 	// this method is executed an a character who is the an opponent of the humen. the playerOpponent object is the human character
 	receiveAttackFromPlayer(playerOpponent) {
-		this.healthPonts -= playerOpponent.currentAttackPower
-
+		console.log("in receiveAttackFromPlayer");
+		console.log("playerOpponent",playerOpponent );
+		this.healthPonts = this.healthPonts - playerOpponent.currentAttackPower;
+		console.log("subtracting ", playerOpponent.currentAttackPower);
+		console.log("healthPoints: ", this.healthPoints);
 		// this player (opponent of human) counterAttackes if not defeated in the attack, but that is handled with the attackWithCharacter method
 	}
 }
@@ -108,6 +117,8 @@ var RPGGame = class {
 		// note that the data in the possibleCharacters array will not be modified, and thos the GameData will not be modified.
 		this.possibleCharacters = GameData.characters;
 		this.gameCharacters = [];
+		this.addEventListeners();
+
 		this.startGame();
 	}
 
@@ -121,12 +132,15 @@ var RPGGame = class {
 
 		$("#character-deck").empty();
 		this.gameCharacters.forEach((character, i) => {
-			console.log("image: ", character.name, character.image)
+			// debugger;
+			console.log(character);
+			console.log("image: ", character.name, character.image, character._id)
 			let characterCard = $("<div>", {
-				class: 'card text-center character-card',
-				id: character.name,
+				class: 'card text-center character-card view overlay hm-blue-light',
+				id: character._id,
 				value: i.toString()
 			});
+			let cardHeader = $("<div>", {class:"card-header", html: character.name});
 			let cardImage = $("<img>", {
 				class: "card-img-top",
 				src: character.image,
@@ -134,10 +148,14 @@ var RPGGame = class {
 			});
 			let cardBody = $("<div>", {
 				class: "card-body"
-			}).append($("<p>", {
-				class: 'card-text',
-				html: character.name
-			}));
+			});
+			// cardBody.append($("<p>", {
+			// 	class: 'card-text',
+			// 	html: character.name
+			// }));
+			let maskDiv = ($("<div>",  {class:'mask'}))
+			// let flexDiv = ($("<div>",  {class:'mask flex-center', html:"Select " + character.name}))
+			// .append("<p>Select Character</p>"));
 
 			let cardFooter = $("<div>", {
 				class: 'card-footer'
@@ -146,17 +164,42 @@ var RPGGame = class {
 				html: "Health: " + character.healthPoints
 			}));
 
-			characterCard.append(cardImage).append(cardBody).append(cardFooter);
+			characterCard.append(cardHeader).append(cardImage).append(cardBody).append(maskDiv).append(cardFooter);
+			// characterCard.append(cardHeader).append(cardImage).append(flexDiv).append(cardFooter);
 			console.log("card: ", characterCard);
 			$("#character-deck").append(characterCard);
 
+
 		});
 
-		this.addEventListeners()
+		// clear the battle characters
+		$("#battle-deck").empty();
+		this.addCharacterClickListeners();
+
+
+		// disable Attack button (will be enabled after player selects charactetr and opponent)
+		$("#attack-button").prop('disabled', true);
+		$("#attack-button").attr('aria-disabled', true);
+
 	}
 
 	setPlayerCharacter(index) {
 		this.humanPlayerCharacterIndex = index;
+		var charObj = this.gameCharacters[this.humanPlayerCharacterIndex];
+		var charCard = $("#" + charObj._id);
+		console.log(charObj, charCard);
+		var cardWidth = charCard.width();
+
+		charCard.animate({width: 0}, "slow", function() {
+			charCard.remove()
+			charCard.appendTo("#battle-deck").animate({width: cardWidth}, "slow");
+			$(".character-card").removeClass("view overlay hm-blue-light");
+
+			$("#character-deck .card").addClass("view overlay hm-red-light")
+
+		});
+
+		// now, make the remaining avaiable players turn reddish when highlighed. Also, don't want player character to highlight
 	}
 
 	setCurrentOpponent(index) {
@@ -164,17 +207,51 @@ var RPGGame = class {
 			// this shouldn't happen.
 			throw "Opponent can't be same character as human player";
 		}
-		this.currentOpponent = index;
+		this.currentOpponentIndex = index;
+
+		var charObj = this.gameCharacters[this.currentOpponentIndex];
+		var charCard = $("#" + charObj._id);
+		console.log(charObj, charCard);
+
+		var cardWidth = charCard.width();
+
+		charCard.animate({width: 0}, "slow", function() {
+			charCard.remove()
+			var newMarginWidth = cardWidth * 2;	
+			// charCard.animate({marginLeft: newMarginWidth})			
+			charCard.appendTo("#battle-deck").animate({width: cardWidth, marginLeft: newMarginWidth}, "slow");
+
+
+			$(".character-card").removeClass("view overlay hm-red-light");
+			$("#attack-button").prop('disabled', false);
+			$("#attack-button").attr('aria-disabled', false);
+			$("#battle-deck small").removeClass('text-muted');
+		});
+
+		// charCard.remove();
+		// charCard.appendTo("#battle-deck");
+
+		// charCard.attr("style: margin-left")
+		
+
+		// now, remove hover highlight from remaining character cards and enabale attack button
+
+
+
 	}
 
 	attackCurrentOpponentWithHumanCharacter() {
 		var humanCharacter = this.gameCharacters[this.humanPlayerCharacterIndex];
-		var opponent = this.currentOpponent;
+		var opponent = this.gameCharacters[this.currentOpponentIndex];
 		humanCharacter.attackWithCharacter(opponent);
 
 		if (humanCharacter.isDefeated()) {
 			$("body").trigger(EventEnum.GAMELOSE);
 		} else if (opponent.isDefeated()) {
+			// if there are no opponents  left, player has won the whole game. Otherwise, player has just won this battle
+			if ($("#character-deck .character-card").length === 0 ) {
+				$("body").trigger(EventEnum.GAMEWIN);				
+			}
 			$("body").trigger(EventEnum.ATTACKWIN);
 		} else {
 			// neither human player nor opponent has lossed. Screen needs to be updated though.
@@ -184,6 +261,9 @@ var RPGGame = class {
 	}
 
 	updateScreen() {
+		// $("#battle-deck .character-card").forEach(function(card) {
+			// console.log("this")
+		// })
 
 	}
 
@@ -196,16 +276,33 @@ var RPGGame = class {
 
 		$("body").on(EventEnum.GAMEWIN, function(e) {
 			// me.startGame();
-			me.updateScreen();
+			// window.alert("Awesome! You won the entire game!");
+			$("#attack-button").prop('disabled', true);
+			$("#attack-button").attr('aria-disabled', true);			
+			// me.updateScreen();
 		});
 
 		$("body").on(EventEnum.GAMELOSE, function(e) {
 			// me.startGame();
-			me.updateScreen();
+			window.alert("Sorry. You lossed");
+			// me.updateScreen();
+		$("#attack-button").prop('disabled', true);
+		$("#attack-button").attr('aria-disabled', true);
+
 		});
 
 		$("body").on(EventEnum.ATTACKWIN, function(e) {
 			// me.startGame();
+			// window.alert("You won a battle!");
+
+			$("#attack-button").prop('disabled', true);
+			$("#attack-button").attr('aria-disabled', true);
+			var opponentCard = $("#battle-deck .character-card:last-child").fadeOut(1000, function() {
+				opponentCard.remove();
+				me.opponentSet = false;
+			});
+			// opponentCard.toggle
+
 			me.updateScreen();
 		});
 
@@ -215,21 +312,42 @@ var RPGGame = class {
 		});
 
 
-		$("#attack").on("click", function() {
 
+		$("#attack-button").on("click", function() {
+			me.attackCurrentOpponentWithHumanCharacter();
+			var charObj = me.gameCharacters[me.humanPlayerCharacterIndex];
+			// var charCard = $("#" + charObj._id);
+
+			var opponentObj = me.gameCharacters[me.currentOpponentIndex];
+			// var opponentCard = $("#" + opponentObj._id);
+
+			$("#" + charObj._id + "  small").text("Health: " + charObj.healthPoints);
+			$("#" + opponentObj._id + " small").text("Health: " + opponentObj.healthPoints);
 		});
 
-		$("#reset").on("click", function() {
+		$("#reset-button").on("click", function() {
+			me.startGame();
+		})
 
-		});
+	}
+
+	addCharacterClickListeners() {
+		var me = this;
 
 		$(".character-card").on("click", function() {
+			console.log("character clicked")
+			console.log("humanPlayerSet", me.humanPlayerSet, ", opponentSet: ", me.opponentSet);
 			if (me.humanPlayerSet === false) {
+				// debugger;
+				console.log($(this).val());
 				var charIndex = Number.parseInt($(this)[0].valueOf().attributes[2].value);
+				// var charIndex = $(this).val();
+
 				// console.log("index", charIndex);
 				me.setPlayerCharacter(charIndex);
 				me.humanPlayerSet = true;
 			} else if (me.opponentSet === false) {
+				console.log("player selected character as opponent")
 				// set opponent index
 				var charIndex = Number.parseInt($(this)[0].valueOf().attributes[2].value);
 				if (charIndex === me.humanPlayerCharacterIndex) {
